8bdd12e7a8cb69d6baebc173be90319d2cd15ddb
emadrigal <emadrigal@apparelinternational.com>
2019-10-24 08:35:20 -0500

    se borra parte del formulario que no se usa

--- application/views/Empleados.php
+++ application/views/Empleados.php
@@ -10,21 +10,13 @@
                 <div class="card">
                     <div class="card-body">     
                         <div class="table-responsive">    
-                            <div class="dataTables_wrapper dt-bootstrap4">    
-                                <div class="row" style="display:none;">              
-                                    <div class="col-md-12 text-right">
-                                        <div class="messages"></div>
-                                        <button id="nuevo" class="btn btn-info" data-toggle="modal" data-target="#myModal_agregar">
-                                            <i class="fa fa-plus-circle"></i> Agregar empleado</button>
-                                       <!-- </button>  -->
-                                    </div> 
-                                </div> 
+                            <div class="dataTables_wrapper dt-bootstrap4">
                                 <div class="row">              
                                     <div class="col-md-12 text-right">
                                         <div class="messages"></div>
                                         <button id="actualizar" class="btn btn-warning" >
                                             <i class="fa fa-refresh"></i> Actualizar Empleados Microsip</button><!-- fa-spin -->
-                                        </button>  
+                                       <!-- </button>  -->
                                     </div> 
                                 </div>      
                                 <div class="row" >
@@ -95,71 +87,7 @@
 <!-- ============================================================== -->    
 <!-- End Container fluid  -->
 <!-- ============================================================== -->
-    <div class="modal fade" id="myModal_agregar" tabindex="-1" role="dialog" aria-labelledby="myModal_categoriasLabel"
-         aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-                                aria-hidden="true">×</span></button>
-                    <h4 class="modal-title" id="myModal_categoriasLabel">AGREGAR</h4></div>
-                <div class="modal-body">
-                    <div class="container">
-                        <form id="formulario" class="cmxform" method="get" action="">
-                        <fieldset>
-                            <input id="id" type="text" style="display:none;">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <label style="font-weight: bold!important;">Nombre:</label> 
-                                        <input id="nombre" type="text" class="form-control required" name="msg" placeholder="Nombre" required>
-                                    </div>
-                                </div>
-                            </div>                        
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <label style="font-weight: bold!important;">Usuario:</label> 
-                                        <input id="usuario" type="text" class="form-control required" name="msg" placeholder="usuario" required>
-                                    </div>
-                                </div>
-                            </div> 
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <label style="font-weight: bold!important;">Contraseña:</label> 
-                                        <input id="password" type="password"  class="form-control required" name="msg" placeholder="constraseña" required>
-                                        <div class="checkbox m-t-20">
-                                            <input id="drop-remove" type="checkbox" onclick="verPassword()">
-                                            <label for="drop-remove" class="col-md-12">
-                                                ver Contraseña
-                                            </label>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-12">                            
-                                    <div class="form-group">
-                                            <label style="font-weight: bold!important;">Perfil</label>
-                                            <select class="custom-select col-12 required" id="perfil" required>
-                                            </select>
-                                        </div>
-                                </div>
-                            </div> 
-                            </fieldset>
-                        </form>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                     <!--<button id="btn_guardar" type="button" class="btn btn-success">Aceptar</button>-->
-                    <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
-                    <button id="btn_guardar" class="submit btn btn-success" type="submit" value="Submit">Guardar</button> 
-                </div>
-            </div>
-        </div>
-    </div>
-
+ 
     <script src="assets/js/empleados.js"></script>
 
 
